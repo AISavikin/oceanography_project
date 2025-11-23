e6c7f9a038913d8cb1ccb38e1bc5466771cb29b8
aisav <aisavikin@gmail.com>
2025-11-23 12:04:25 +0300

    вьюхи и шаблоны expedition_list и expedition_detail

--- oceanography/urls.py
+++ oceanography/urls.py
@@ -6,5 +6,7 @@ app_name = 'oceanography'
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('coming-soon/', ComingSoonView.as_view(), name='coming_soon'),
+    path('expeditions/', ExpeditionListView.as_view(), name='expedition_list'),
+    path('expeditions/<int:pk>/', ExpeditionDetailView.as_view(), name='expedition_detail'),
 ]
     
--- oceanography/views.py
+++ oceanography/views.py
@@ -1,4 +1,5 @@
-from django.views.generic import TemplateView, ListView
+from django.views.generic import TemplateView, ListView, DetailView
+from django.urls import reverse
 from .models import (
     Expedition, Station, Sample, MeteoData, CarbonData, 
     IonicCompositionData, PigmentsData, OxymetrData, 
@@ -28,4 +29,40 @@ class HomeView(ListView):
         context['samples_count'] = Sample.objects.count()
         context['ctd_data_count'] = CTDData.objects.count()
         
+        return context
+
+class ExpeditionListView(ListView):
+    model = Expedition
+    template_name = 'oceanography/expedition_list.html'
+    context_object_name = 'expeditions'
+    paginate_by = 20
+    
+    def get_queryset(self):
+        return Expedition.objects.all().prefetch_related('stations')
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['breadcrumbs'] = [
+            {'url': reverse('oceanography:home'), 'name': 'Главная'},
+            {'url': '', 'name': 'Экспедиции'}
+        ]
+        return context
+
+class ExpeditionDetailView(DetailView):
+    model = Expedition
+    template_name = 'oceanography/expedition_detail.html'
+    context_object_name = 'expedition'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        expedition = self.get_object()
+        
+        context['stations'] = expedition.stations.all().prefetch_related('samples')
+        context['samples_count'] = Sample.objects.filter(station__expedition=expedition).count()
+        
+        context['breadcrumbs'] = [
+            {'url': reverse('oceanography:home'), 'name': 'Главная'},
+            {'url': reverse('oceanography:expedition_list'), 'name': 'Экспедиции'},
+            {'url': '', 'name': f'{expedition.platform} ({expedition.start_date.year})'}
+        ]
         return context
