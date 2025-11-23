b69d49457f1b7b3a2baab88f3151f16a4ae545e3
aisav <aisavikin@gmail.com>
2025-11-23 12:16:32 +0300

    Экспдиции в админке

--- oceanography/admin.py
+++ oceanography/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Expedition
 
+@admin.register(Expedition)
+class ExpeditionAdmin(admin.ModelAdmin):
+    list_display = ('platform', 'start_date', 'end_date', 'area', 'stations_count')
+    list_filter = ('platform', 'start_date', 'area')
+    search_fields = ('platform', 'area')
+    date_hierarchy = 'start_date'
+    ordering = ('-start_date',)
+    
+    fieldsets = (
+        ('Основная информация', {
+            'fields': ('platform', 'area')
+        }),
+        ('Временные параметры', {
+            'fields': ('start_date', 'end_date')
+        }),
+    )
+    
+    def stations_count(self, obj):
+        return obj.stations.count()
+    stations_count.short_description = 'Кол-во станций'
 # Register your models here.
